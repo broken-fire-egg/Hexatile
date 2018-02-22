cd9c4bd6e02af301e6ed7aeecacf21c4a5a69921
broken-fire-egg <36439168+broken-fire-egg@users.noreply.github.com>
2018-02-22 21:48:08 +0900

    Add files via upload

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+    public Sprite TileSprite;
+    public int width;
+    public int height;
+    public GameObject TileObject;
+    public Tiles[,] TileMap;
+    
+    // Use this for initialization
+    void Start() {
+        Init();
+        foreach(Tiles element in TileMap[1, 1].ControllTiles(Tiles.Ways.Right, 5))
+        {
+            element.GetComponent<SpriteRenderer>().color = Color.black;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void Init()
+    {
+        TileMap = new Tiles[width, height];
+        for (int i = 0; i < width; i++)
+        {
+            for (int k = 0; k < height; k++)
+            {
+                GameObject GO = Instantiate(TileObject, new Vector3((k % 2 == 0) ? i + 0.5f : i, -k * 0.86f), new Quaternion(), gameObject.transform);
+                TileMap[i, k] = GO.GetComponent<Tiles>();
+                TileMap[i, k].pos_x = i;
+                TileMap[i, k].pos_y = k;
+            }
+        }
+        for (int i = 0; i < width; i++)
+        {
+            for(int k=0; k < height; k++)
+            {
+                if(k % 2 == 0)
+                {
+
+                    TileMap[i, k].ArroundTiles[0] = TileMap[(i + 1 < width) ? i + 1 : i, (k > 0) ? k - 1 : k];
+                    TileMap[i, k].ArroundTiles[2] = TileMap[(i + 1 < width) ? i + 1 : i, (k + 1 < height) ? k + 1 : k];
+                    TileMap[i, k].ArroundTiles[3] = TileMap[i, (k + 1 < height) ? k + 1 : k];
+                    TileMap[i, k].ArroundTiles[5] = TileMap[i, (k > 0) ? k - 1 : k];
+                }
+                else
+                {
+                TileMap[i, k].ArroundTiles[0] = TileMap[i, (k > 0) ? k - 1 : k];
+                TileMap[i, k].ArroundTiles[2] = TileMap[i, (k + 1 < height) ? k + 1 : k];
+                TileMap[i, k].ArroundTiles[3] = TileMap[(i > 0) ? i - 1 : i, (k + 1 < height) ? k + 1 : k];
+                TileMap[i, k].ArroundTiles[5] = TileMap[(i > 0) ? i - 1 : i, (k > 0) ? k - 1 : k];
+                }
+                TileMap[i, k].ArroundTiles[1] = TileMap[(i + 1 < width) ? i + 1 : i, k];
+                TileMap[i, k].ArroundTiles[4] = TileMap[(i > 0) ? i - 1 : i, k];
+
+                if (i == 0)
+                {
+                    TileMap[i, k].ArroundTiles[4] = TileMap[i, k];
+                    if (k % 2 == 1)
+                    {
+                        TileMap[i, k].ArroundTiles[5] = TileMap[i, k];
+                        TileMap[i, k].ArroundTiles[3] = TileMap[i, k];
+                    }
+                }
+                if (i == width - 1)
+                {
+                    TileMap[i, k].ArroundTiles[1] = TileMap[i, k];
+                    if (k % 2 == 0)
+                    {
+                        TileMap[i, k].ArroundTiles[0] = TileMap[i, k];
+                        TileMap[i, k].ArroundTiles[2] = TileMap[i, k];
+                    }
+                }
+                if (k == 0)
+                {
+                    TileMap[i, k].ArroundTiles[0] = TileMap[i, k];
+                    TileMap[i, k].ArroundTiles[5] = TileMap[i, k];
+                }
+                if (k == height - 1)
+                {
+                    TileMap[i, k].ArroundTiles[2] = TileMap[i, k];
+                    TileMap[i, k].ArroundTiles[3] = TileMap[i, k];
+                }
+
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Tiles.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiles : MonoBehaviour {
+    public enum Ways { RightUp,Right,RightDown,LeftDown,Left,LeftUp }
+    public int value;
+    public int pos_x;
+    public int pos_y;
+    public Tiles[] ArroundTiles = new Tiles[6];
+
+    public static Tiles[] operator+(Tiles[] _tiles,Tiles _tile)
+    {
+        if (_tiles == null)
+        {
+            Tiles[] tmp = new Tiles[1];
+            tmp[0] = _tile;
+            return tmp;
+        }
+        if(_tile == null)
+        {
+            return _tiles;
+        }
+
+        Tiles[] result = new Tiles[_tiles.Length + 1];
+        result[0] = _tile;
+
+        for(int i=1;i< result.Length;i++)
+            result[i] = _tiles[i - 1];
+
+        return result;
+    }
+    public static Tiles[] operator +(Tiles tile1, Tiles tile2)
+    {
+        if (tile1 == null|| tile2 == null)
+            return null;
+       
+
+        Tiles[] result = new Tiles[2];
+        result[0] = tile2;
+        result[1] = tile1;
+        return result;
+    }
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Tiles[] ControllTiles(Ways way,int size,bool Bi_direction = false)
+    {
+        Tiles[] result = new Tiles[size];
+        result = SearchTiles(null, way, size);
+        return result;
+    }
+    Tiles[] SearchTiles(Tiles[] Searched,Ways way,int size)
+    {
+        if(size == 0)
+          return null;
+
+        Debug.Log(size.ToString());
+
+        if(ArroundTiles[(int)way] == this)
+            return ArroundTiles[(int)way].SearchTiles(Searched, way, 0) + this;
+
+        return ArroundTiles[(int)way].SearchTiles(Searched,way,size-1) + this;
+
+    }
+}
